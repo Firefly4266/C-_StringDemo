57cc60892cefc34c86f79ac586b39de2c669abad
Firefly4266 <gaines.johnp@gmail.com>
2017-08-24 22:38:32 -0700

    completed string demo

---
+++ StringDemo/StringDemo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+public class StringDemo
+{
+    public static void Main(string[] args)
+    {
+        string cmd;
+        string buffer;
+        string buffer2;
+        iw = new InputWrapper();
+        buffer = iw.getString("Enter string to work on: ");
+        Console.WriteLine("Enter command, 'quit' to exit");
+        cmd = iw.getString("> ");
+        while (!cmd.Equals("quit"))
+        {
+            if (cmd.Equals("length"))
+            {
+                Console.WriteLine("length = " + buffer.Length);
+            }
+            else if (cmd.Equals("new"))
+            {
+                buffer = iw.getString("new string: ");
+            }
+            else if (cmd.Equals("show"))
+            {
+                show(buffer);
+            }
+            else if (cmd.Equals("upper"))
+            {
+                buffer = buffer.ToUpper();
+                show(buffer);
+            }
+            else if (cmd.Equals("lower"))
+            {
+                buffer = buffer.ToLower();
+                show(buffer);
+            }
+            else if (cmd.Equals("append"))
+            {
+                buffer2 = iw.getString("Enter second string: ");
+                buffer = buffer2;
+                show(buffer);
+            }
+            else if (cmd.Equals("substring"))
+            {
+                substring(buffer);
+            }
+            else if (cmd.Equals("index"))
+            {
+                index(buffer);
+            }
+            else if (cmd.Equals("char"))
+            {
+                character(buffer);
+            }
+            else if (cmd.Equals("reverse"))
+            {
+                buffer = reverse(buffer);
+            }
+            else
+                help();
+            cmd = iw.getString("> ");
+        }
+    }
+    private static void show(string buffer)
+    {
+        Console.WriteLine("Current string = " + buffer);
+    }
+    private static void substring(string buffer)
+    {
+        int begin = iw.getInt("Enter beginning index: ");
+        int length = iw.getInt("Enter length of substring: ");
+        string buffer2 = buffer.Substring(begin, length);
+        Console.WriteLine("Substring = " + buffer2);
+    }private static void index(String buffer)
+    {
+        String buffer2 = iw.getString("Enter second string.");
+        int index = buffer.IndexOf(buffer2);
+        Console.WriteLine("index = " + index);
+    }
+    private static void character(String buffer)
+    {
+        int index = iw.getInt("Enter index ");
+        char ch = buffer[index];
+        Console.WriteLine("Character = " + ch);
+    }
+    private static String reverse(String buffer)
+    {
+        String reverse = "";
+        for (int i = buffer.Length - 1; i >= 0; i--)
+            reverse += buffer[i];
+            Console.WriteLine("Reversed string = " + reverse);
+            return reverse;
+    }
+    private static void help()
+    {
+        Console.WriteLine("legal commands are:");
+        Console.WriteLine("\tlength");
+        Console.WriteLine("\tnew");
+        Console.WriteLine("\tshow");
+        Console.WriteLine("\tupper");
+        Console.WriteLine("\tlower");
+        Console.WriteLine("\tappend");
+        Console.WriteLine("\tsubstring");
+        Console.WriteLine("\tindex");
+        Console.WriteLine("\tchar");
+        Console.WriteLine("\treverse");
+        Console.WriteLine("\tquit");
+    }
+
+    static InputWrapper iw;
+}
